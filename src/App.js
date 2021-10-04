54be06b14b4a54d12d76c915c065f0728ecd6b09
Sawee Rawal <saweerawal1998@gmail.com>
2021-10-04 15:11:40 +0530

    added description

--- src/App.js
+++ src/App.js
@@ -2,24 +2,78 @@ import React, { useState } from "react";
 import "./styles.css";
 
 var party_songs = [
-  { song: "STAY (Justin Bieber)", rating: "5/5" },
-  { song: "Oh Ho Ho Ho(Remix)", rating: "5/5" },
-  { song: "Dheeme Dheeme", rating: "4.5/5" },
-  { song: "Brown Munde", rating: "4.5/5" }
+  {
+    song: "STAY (Justin Bieber)",
+    desc:
+      "Stay(stylized in all caps) is a song by Australian rapper and singer the Kid Laroi and Canadian singer Justin Bieber. ",
+    rating: "5/5"
+  },
+  {
+    song: "Oh Ho Ho Ho(Remix)",
+    desc:
+      "Oh Ho Ho Ho (Remix) composed by Sukhbir, Abhijit Vaghani and sung by Sukhbir from the Bollywood film Hindi Medium",
+    rating: "5/5"
+  },
+  {
+    song: "Dheeme Dheeme",
+    desc: "Dheeme Dheeme is a Hindi song by Tony Kakkar",
+    rating: "4.5/5"
+  },
+  {
+    song: "Brown Munde",
+    desc:
+      "Punjabi song 'Brown Munde' sung by AP Dhillon, Gurinder Gill and Shinda Kahlon.",
+    rating: "4.5/5"
+  }
 ];
 
 var romantic_songs = [
-  { song: "Raataan Lambiyan", rating: "5/5" },
-  { song: "Dil Ko Karaar Aaya", rating: "5/5" },
-  { song: "Tu Hi Yaar Mera", rating: "4.5/5" },
-  { song: "Sona Lagda", rating: "4.0/5" }
+  {
+    song: "Raataan Lambiyan",
+    desc: "'Raataan Lambiyan' is sung by Jubin Nautiyal & Asees Kaur",
+    rating: "5/5"
+  },
+  {
+    song: "Dil Ko Karaar Aaya",
+    desc: "Dil Ko Karaar Aaya (From 'Sukoon') song sung by Neha Kakkar ",
+    rating: "5/5"
+  },
+  {
+    song: "Tu Hi Hai Meri Zindagi",
+    desc: "'Tu Hi Hai Meri Zindagi' sung by Nikhil D’Souza.",
+    rating: "4.5/5"
+  },
+  {
+    song: "Sona Lagda",
+    desc:
+      "Sona Lagda Sung by Sukh-E, Sukriti Kakar, Prakriti Kakar and composed by Bharatt-Saurabh.",
+    rating: "4.0/5"
+  }
 ];
 
 var sad_songs = [
-  { song: "Mann Bharryaa 2.0", rating: "5/5" },
-  { song: "Yeh Dooriyan", rating: "5/5" },
-  { song: "Shayad", rating: "4.5/5" },
-  { song: "Mehrama", rating: "3.0/5" }
+  {
+    song: "Mann Bharryaa 2.0",
+    desc: "'Mann Bharryaa 2.0' is sung by B Praak.",
+    rating: "5/5"
+  },
+  {
+    song: "Yeh Dooriyan",
+    desc:
+      "Yeh Dooriyan is a Hindi song and is sung by Pritam and Mohit Chauhan.",
+    rating: "5/5"
+  },
+  {
+    song: "Shayad",
+    desc:
+      " 'Shayad' is sung by Arijit Singh and music of the song is given by Pritam.",
+    rating: "4.5/5"
+  },
+  {
+    song: "Mehrama",
+    desc: "'Mehrama' is sung by Darshan Raval & Antara Mitra.",
+    rating: "3.0/5"
+  }
 ];
 
 export default function App() {
@@ -77,7 +131,12 @@ export default function App() {
               >
                 {song["song"]}
                 <p>
-                  <small>rating: {song["rating"]}</small>
+                  <p>
+                    <small>{song["desc"]}</small>
+                  </p>
+                  <p>
+                    <small>rating: {song["rating"]}</small>
+                  </p>
                 </p>
               </li>
             );
